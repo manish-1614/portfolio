0f35bdcd0ca06df0c95986c74dbe28fb1fe54fdf
Manish Kumar Prajapati <myindiaman@gmail.com>
2026-03-04 23:54:43 +0530

    build: Configure Next.js for GitHub Pages deployment by adding `.nojekyll` and setting `basePath` and `assetPrefix`.

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,8 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
   output: isProd ? 'export' : undefined,
-  basePath: isProd ? '/portfolio' : '',
+  basePath: '/ManishPrajapati',
+  assetPrefix: '/ManishPrajapati/',
   images: {
     unoptimized: true,
   }
