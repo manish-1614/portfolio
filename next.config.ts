c5d4d8e4182e3a6cb456d8c2397225d0a227e2b6
Manish Kumar Prajapati <myindiaman@gmail.com>
2026-03-04 23:58:56 +0530

    feat: Update `basePath` to be conditional and remove `assetPrefix` configuration.

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   /* config options here */
   output: isProd ? 'export' : undefined,
-  basePath: '/ManishPrajapati',
-  assetPrefix: '/ManishPrajapati/',
+  basePath: isProd ? '/portfolio' : '',
   images: {
     unoptimized: true,
   }
